d1e1812b5f84284ff7f94b9f4b2b944d84a5bad3
Oscar <fu.wire@gmail.com>
2021-11-30 09:10:37 -0600

    chore: use default config

--- actions/asccicast/src/cli.ts
+++ actions/asccicast/src/cli.ts
@@ -4,11 +4,17 @@ import * as fs from "fs";
 
 const yargs = require('yargs');
 
+const configFile = process.env.ASCIINEMA_CONFIG_FILE || './config.sample.json'
+
+// use default configuration from file
+const defaultConfig = JSON.parse(fs.readFileSync(path.resolve(configFile), 'utf-8'))
+
 const argv = yargs
     .commandDir('cmds')
     .config('c', function (configPath: string) {
         return JSON.parse(fs.readFileSync(path.resolve(configPath), 'utf-8'))
     })
+    .config(defaultConfig)
     .demandCommand()
     .strictCommands(true)
     .strictOptions(false)
--- actions/asciicast2gif/entrypoint.sh
+++ actions/asciicast2gif/entrypoint.sh
@@ -2,9 +2,9 @@
 
 mkdir -p .asciicast
 
-for filename in ${ASCCINEMA_GLOB_EXP}; do
+for filename in ${ASCIINEMA_GLOB_EXP}; do
   echo "=== PROCESSING: $filename ==="
   GIF_FILENAME=${filename/json/gif}
-  NODE_TLS_REJECT_UNAUTHORIZED=0 /app/asciicast2gif -t ${ASCCINEMA_THEME} -s ${ASCCINEMA_SPEED} -S ${ASCCINEMA_SCALE} -w ${ASCCINEMA_COLUMNS} -h ${ASCCINEMA_ROWS} $filename .asciicast/${GIF_FILENAME}
+  NODE_TLS_REJECT_UNAUTHORIZED=0 /app/asciicast2gif -t ${ASCIINEMA_THEME} -s ${ASCIINEMA_SPEED} -S ${ASCIINEMA_SCALE} -w ${ASCIINEMA_COLUMNS} -h ${ASCIINEMA_ROWS} $filename .asciicast/${GIF_FILENAME}
   echo "=== FINISHED: ${GIF_FILENAME} ==="
 done
