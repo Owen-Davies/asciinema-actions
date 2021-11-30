30a97b1c810db5b588639d9a51788e7593732c69
Oscar <fu.wire@gmail.com>
2021-11-29 22:12:38 -0600

    chore: remove -w & -h flags

--- actions/asciicast2gif/entrypoint.sh
+++ actions/asciicast2gif/entrypoint.sh
@@ -5,6 +5,7 @@ mkdir -p .asccicast
 for filename in ${ASCCINEMA_GLOB_EXP}; do
   echo "=== PROCESSING: $filename ==="
   GIF_FILENAME=${filename/json/gif}
-  NODE_TLS_REJECT_UNAUTHORIZED=0 /app/asciicast2gif -t ${ASCCINEMA_THEME} -s ${ASCCINEMA_SPEED} -S ${ASCCINEMA_SCALE} -w ${ASCCINEMA_COLUMNS} -h ${ASCCINEMA_ROWS} $filename .asccicast/${GIF_FILENAME}
+  # -w ${ASCCINEMA_COLUMNS} -h ${ASCCINEMA_ROWS}
+  NODE_TLS_REJECT_UNAUTHORIZED=0 /app/asciicast2gif -t ${ASCCINEMA_THEME} -s ${ASCCINEMA_SPEED} -S ${ASCCINEMA_SCALE} $filename .asccicast/${GIF_FILENAME}
   echo "=== FINISHED: ${GIF_FILENAME} ==="
 done
